d5bbda599a48016af927abe029051a025791b192
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-07-15 20:32:20 +0300

    feat: add movies controllers

---
+++ controllers/movies.js
@@ -0,0 +1,45 @@
+import Movie from '../models/movie';
+import BadRequestError from '../errors/BadRequestError';
+import NotFoundError from '../errors/NotFoundError';
+import ForbiddenError from '../errors/ForbiddenError';
+
+const getMovies = (req, res, next) => {
+  Movie.find({})
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+const createMovie = (req, res, next) => {
+  const { _id: owner } = req.user;
+  Movie.create({ ...req.body, owner })
+    .then((movie) => res.status(201).send(movie))
+    .catch((err) => {
+      if (err.message.includes('validation failed')) {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.id)
+    .orFail(() => new NotFoundError('Фильм с переаднным id не найден'))
+    .then((movie) => {
+      if (movie.owner.equals(req.user._id)) {
+        movie.deleteOne();
+        return movie;
+      }
+      throw new ForbiddenError('Удалить фильм может только его владелец');
+    })
+    .then((movie) => res.send(movie))
+    .catch((err) => {
+      if (err.message.includes('Cast to ObjectId failed')) {
+        next(new BadRequestError('Передан некорректный id'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+export { getMovies, createMovie, deleteMovie };
--- errors/ConflictError.js
+++ errors/ConflictError.js
@@ -6,4 +6,4 @@ class ConflictError extends Error {
   }
 }
 
-module.exports = ConflictError;
+export default ConflictError;
