61f6b5e419888ef3452f5af88e9f4f26c2070338
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-07-16 15:06:59 +0300

    fix: delete pr

--- errors/NotFoundError.js
+++
@@ -1,9 +0,0 @@
-class NotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'BadRequestError';
-    this.statusCode = 404;
-  }
-}
-
-module.exports = NotFoundError;
--- utils/constants.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-useless-escape
-const LINK_REGEX = /https*:\/\/(www.)*[a-z0-9\-\.]{1,}\.[a-z]{2,3}[a-z0-9\-\._~\:\/\?\#\[\]@\!\$&'\(\)\*\+,;\=]*/;
-
-module.exports = LINK_REGEX;
